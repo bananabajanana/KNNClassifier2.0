4deffd566d567e5eb5e540c12a650e7025ace738
bananabajanana <ohad.heines@gmail.com>
2022-08-14 19:24:29 +0300

    Added todo and a draft of the server-client interaction diagram to current main.cpp (will need to be moved later

--- Server/Source/main.cpp
+++ Server/Source/main.cpp
@@ -6,6 +6,28 @@
 #include "source/srcKnnClassifier/Classifier.hpp"
 #include "source/srcDistances/DistancesData.hpp"
 
+/*
+ * TODO:
+ *   -Write server-side and client-side main codes
+ *   -Make new srcIO for server-side (instead of converting file, convert input from client)
+ *   -Make the server-side return immediately the class after getting a flower
+ *   -Make the client-side read the input file one at a time, and after sending each flower to the server write the returned class to the output
+ *   -Update all file calls to updated places (post directory-reorganization)
+ *   -Update CMakeLists to work with two executables (server-side and client-side)
+ */
+
+/*
+ * Server-Client interaction diagram.
+ *
+ *   Client:
+ *             Sends one flower  <--   Updates output file
+ *             info to server.         with new classification
+ *                 |                        ^
+ *   Server:       V                        |
+ *             Turns info to     -->   Classifies flower
+ *             Flower object           and sends info to client
+ */
+
 int main(int argc, char* argv[]) {
     //loading input files and k
     FileConverter fc;
