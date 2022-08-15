82bd94b68cf1c7a14858e75bf040961926a089e7
מיכל יעקובשוילי <michal.iakobashvili@ashdod.eduil.org>
2022-08-15 22:05:32 +0300

    did some changes in server

--- Server/Source/main.cpp
+++ Server/Source/main.cpp
@@ -41,7 +41,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
     FileConverter fc;
-    std::vector<Flower> classified = fc.updateFromFile("../input/classified.csv");
+    std::vector<Flower> classified = fc.updateFromFile("../Server/Data/classified.csv");
     int k = 5;
     Classifier machine(k, classified);
 
@@ -59,42 +59,41 @@ int main(int argc, char* argv[]) {
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("error binding socket");
     }
-    if (listen(sock, 5) < 0) {
-        perror("error listening to a socket");
+    while(true) {
+        if (listen(sock, 5) < 0) {
+            perror("error listening to a socket");
+        }
+        struct sockaddr_in client_sin;
+        unsigned int addr_len = sizeof(client_sin);
+        int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
+        if (client_sock < 0) {
+            perror("error accepting client");
+        }
+        // endregion
+
+        char buffer[4096];
+        int expected_data_len = sizeof(buffer);
+        int read_bytes;
+        do {
+            read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+            if (read_bytes == 0) {
+                break;
+                // connection is closed
+            } else if (read_bytes < 0) {
+                perror("There was a problem in recieving the information.");
+                // error
+            }
+            Flower unclassified = fc.flowerFromLine(buffer);
+            DistanceCalc *d = ((DistanceCalc *) new EuclideanDistance);
+            machine.defFlower(unclassified, *d);
+            int sent_bytes = send(client_sock, fc.getType(unclassified.getTypeOfIris()), read_bytes, 0);
+
+            if (sent_bytes < 0) {
+                perror("error sending to client");
+            }
+        } while (read_bytes != 0);
+
     }
-    struct sockaddr_in client_sin;
-    unsigned int addr_len = sizeof(client_sin);
-    int client_sock = accept(sock,  (struct sockaddr *) &client_sin,  &addr_len);
-    if (client_sock < 0) {
-        perror("error accepting client");
-    }
-    //endregion
-
-    char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    int read_bytes =1;
-    do {
-        read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-        if (read_bytes == 0) {
-            // connection is closed
-        }
-        else if (read_bytes < 0) {
-            // error
-        }
-        else {
-            //cout << buffer;
-        }
-        Flower unclassified = fc.flowerFromLine(buffer);
-        DistanceCalc* d = ((DistanceCalc *)new EuclideanDistance);
-        machine.defFlower(unclassified, *d);
-        int sent_bytes = send(client_sock, fc.getType(unclassified.getTypeOfIris()), read_bytes, 0);
-
-        if (sent_bytes < 0) {
-            perror("error sending to client");
-        }
-    } while(read_bytes != 0);
-
-
     close(sock);
 
 
