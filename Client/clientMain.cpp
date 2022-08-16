ecd405177e5906729331723eab133130247c818e
bananabajanana <ohad.heines@gmail.com>
2022-08-16 21:39:02 +0300

    Updated readme

--- Client/clientMain.cpp
+++ Client/clientMain.cpp
@@ -1,11 +1,13 @@
 #include "Client.hpp"
 #define CLIENT_PORT 6969
 #define MY_IP "127.0.0.1"
-int main(int argc, char **argv) {
-    ClientProcess c(MY_IP, CLIENT_PORT);
 
-    if(argc >= 2) {
-        c.setPaths(argv[0], argv[1]);
+
+int main(int argc, char **argv) {
+    ClientProcess c((char *)MY_IP, CLIENT_PORT);
+
+    if(argc >= 3) {
+        c.setPaths(argv[1], argv[2]);
     }
 
     c.runClient();
